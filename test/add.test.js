203da2a7a32cf5601129568ce76f0db6b409a37a
benjamin-sh <viaudbenjamin@gmail.com>
2019-01-17 15:08:44 +0100

    writing firsts tests

--- test/add.test.js
+++ test/add.test.js
@@ -1,20 +1,27 @@
-const test = require('ava');
-const add = require('.');
+import add from '../src/add';
 
-test('test add numbers', t => {
-	t.is(add(6, 4), 10);
-	t.is(add(6.5, 4.5), 11);
-	t.is(add(6, -4), 2);
-	t.is(add(6, -8), -2);
-	t.is(add(6, ''), '6');
-	t.is(add(6, undefined), NaN);
-	t.is(add(6, null), 6);
-	t.is(add(6, true), 7);
-	t.is(add(6, false), 6);
-	t.is(add(6, '4'), '64');
-	t.is(add(6, {}), NaN);
-	t.is(add(6, new Object()), NaN); // eslint-disable-line no-new-object
-	t.is(add(6, new RegExp()), NaN);
-	t.is(add(6, Object(2)), 8); // eslint-disable-line unicorn/new-for-builtins
-	t.is(add(6, Object('2')), '62'); // eslint-disable-line unicorn/new-for-builtins
+describe('Utilities', () => {
+  describe('add()', () => {
+    test('should return 10 when passed 6 + 4', () => {
+      expect(add(6, 4)).toBe(10);
+    });
+  });
 });
+
+// test('test add numbers', t => {
+// t.is(add(6, 4), 10);
+// t.is(add(6.5, 4.5), 11);
+// t.is(add(6, -4), 2);
+// t.is(add(6, -8), -2);
+// t.is(add(6, ''), '6');
+// t.is(add(6, undefined), NaN);
+// t.is(add(6, null), 6);
+// t.is(add(6, true), 7);
+// t.is(add(6, false), 6);
+// t.is(add(6, '4'), '64');
+// t.is(add(6, {}), NaN);
+// t.is(add(6, new Object()), NaN); // eslint-disable-line no-new-object
+// t.is(add(6, new RegExp()), NaN);
+// t.is(add(6, Object(2)), 8); // eslint-disable-line unicorn/new-for-builtins
+// t.is(add(6, Object('2')), '62'); // eslint-disable-line unicorn/new-for-builtins
+// });
--- test/isObj.test.js
+++ test/isObj.test.js
@@ -1,25 +1,32 @@
-const test = require('ava');
-const isObj = require('.');
+import isObj from '../src/isObj';
 
-test('test isObj with string', t => {
-	t.true(isObj({}));
-	t.true(isObj(new Object())); // eslint-disable-line no-new-object
-	t.true(isObj(new Date()));
-	t.true(isObj(new RegExp()));
-	t.true(isObj(Object.create(null)));
-	t.true(isObj({foo: true}));
-	t.true(isObj([]));
-	t.true(isObj(['foo', 'bar']));
-	t.true(isObj(() => {}));
-	t.true(isObj(arguments)); // eslint-disable-line no-undef
-	t.true(isObj(/./));
-	t.true(isObj(Object(0))); // eslint-disable-line  unicorn/new-for-builtins
-	t.true(isObj(Object('foo'))); // eslint-disable-line  unicorn/new-for-builtins
-	t.true(isObj(Object(false))); // eslint-disable-line  unicorn/new-for-builtins
-	t.false(isObj(null));
-	t.false(isObj(undefined));
-	t.false(isObj(NaN));
-	t.false(isObj(''));
-	t.false(isObj(0));
-	t.false(isObj(false));
+describe('Utilities', () => {
+  describe('isObj()', () => {
+    test('should return true when passed an object literal', () => {
+      expect(isObj({})).toBe(true);
+    });
+  });
 });
+
+// test('test isObj with string', t => {
+// t.true(isObj({}));
+// t.true(isObj(new Object())); // eslint-disable-line no-new-object
+// t.true(isObj(new Date()));
+// t.true(isObj(new RegExp()));
+// t.true(isObj(Object.create(null)));
+// t.true(isObj({foo: true}));
+// t.true(isObj([]));
+// t.true(isObj(['foo', 'bar']));
+// t.true(isObj(() => {}));
+// t.true(isObj(arguments)); // eslint-disable-line no-undef
+// t.true(isObj(/./));
+// t.true(isObj(Object(0))); // eslint-disable-line  unicorn/new-for-builtins
+// t.true(isObj(Object('foo'))); // eslint-disable-line  unicorn/new-for-builtins
+// t.true(isObj(Object(false))); // eslint-disable-line  unicorn/new-for-builtins
+// t.false(isObj(null));
+// t.false(isObj(undefined));
+// t.false(isObj(NaN));
+// t.false(isObj(''));
+// t.false(isObj(0));
+// t.false(isObj(false));
+// });
